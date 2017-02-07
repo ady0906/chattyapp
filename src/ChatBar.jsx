72779636102145a9614ad3e97adb77ed14323cb0
Adrien Peynichou <adrien.peynichou@gmail.com>
2017-02-07 21:35:04 +0000

    pushing the work from the first two days of chatty

---
+++ src/ChatBar.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+
+class ChatBar extends Component {
+
+  render() {
+    return (
+      <footer className="chatbar">
+        <input className="chatbar-username" placeholder= "Enter your name here" defaultValue= {this.props.currentUser.name} />
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+      </footer>
+    );
+  }
+}
+export default ChatBar;
---
+++ src/MessageList.jsx
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import Message from './Message.jsx';
+
+class MessageList extends Component {
+  render() {
+    let results = this.props.messages;
+    console.log(results);
+    return (
+      <main className="messages">
+        <ul>
+            {results.map(function(result) {
+              return (
+                <li key={result.id}>
+                  <div className="message-username">{result.username}</div><div className="message-content">{result.content}</div>
+                </li>
+                
+                )
+              })}
+        </ul>
+      </main>
+    );
+  }
+}
+export default MessageList;
