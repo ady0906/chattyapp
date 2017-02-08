741f5ce190722d42a5549c8645ad9b5e8d390044
Adrien Peynichou <adrien.peynichou@gmail.com>
2017-02-08 00:20:59 +0000

    got the responsive chat to work with event handler function

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import MessageList from './MessageList.jsx';
 
 import ChatBar from './ChatBar.jsx';
 
+
 let data = {
   currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
   messages: [
@@ -20,37 +21,47 @@ let data = {
 };
 
 
-  class App extends Component {
+class App extends Component {
 
-    constructor(props) {
-      super(props);
-      this.state = data;
-    };
+  constructor(props) {
+    super(props);
+    this.state = data;
+  };
 
-componentDidMount() {
-  console.log("componentDidMount <App />");
-  setTimeout(() => {
-    console.log("Simulating incoming message");
-    // Add a new message to the list of messages in the data store
-    const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-    const messages = this.state.messages.concat(newMessage)
-    // Update the state of the app component.
-    // Calling setState will trigger a call to render() in App and all child components.
-    this.setState({messages: messages})
-  }, 3000);
-}
 
+  handlePressEnter = e => {
+    if (e.key === "Enter") {
+      const newMessage = {id: this.state.messages.length + 1, username: 'Bob', content: e.target.value};
+      const messages = this.state.messages.concat(newMessage);
+      this.setState({messages: messages})
+    } else {
+      console.log('nope');
+    }
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+    }, 3000);
+  }
 
   render() {
-
 
       return (
         <div>
           <h1>Chatty</h1>
           <MessageList messages={this.state.messages} />
-          <ChatBar currentUser={this.state.currentUser}/>
+          <ChatBar currentUser={this.state.currentUser} handlePressEnter={this.handlePressEnter} />
         </div>
       );
     }
   }
+
   export default App;
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -8,7 +8,7 @@ class ChatBar extends Component {
     return (
       <footer className="chatbar">
         <input className="chatbar-username" placeholder= "Enter your name here" defaultValue= {this.props.currentUser.name} />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyPress= {this.props.handlePressEnter} />
       </footer>
     );
   }
